71b9eddab3c4d995d06417c57abf9927bbd48df9
Mohamed amine marnissi <35928931+AmineMarnissi@users.noreply.github.com>
2023-03-11 13:30:08 +0100

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 model="./best_model_tr.pth"
-video_read=$0
-video_write=$1
 
-python demo.py --load_name ${model} --video_read ${video_read} --video_write ${video_write} 
+python demo.py --load_name ${model} --video_read $1 --video_write $2
